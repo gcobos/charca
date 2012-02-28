8e355a69729446e15cd1e0dcf308a47a69bd4841
Gonzalo <gonzalo@drupro.fi>
2012-02-28 20:57:45 +0200

    sdfdsf

--- index.php
+++ index.php
@@ -200,6 +200,62 @@ $facebook = new Facebook($config);
   parse_str($token_response, $params);
   $app_access_token = $params['access_token'];
 
+
+
+  //Get Score **************
+  if(isset($_COOKIE['Sscore']))
+        $cscore = $_COOKIE['Sscore'];
+else
+        exit;
+
+
+$score=$cscore;
+  // POST a user score
+  //print('Publish a User Score<br/>');
+  $score_URL = 'https://graph.facebook.com/' . $uid . '/scores';
+  $score_result = https_post($score_URL,
+    'score=' . $score
+    . '&access_token=' . $app_access_token
+  );
+ printf('<br/>%s<br/>',$score_result);
+
+  function https_post($uri, $postdata) {
+    $ch = curl_init($uri);
+   // curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
+    $result = curl_exec($ch);
+    curl_close($ch);
+
+    return $result;
+  }
+  function parse_signed_request($signed_request, $secret) {
+    list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+    // decode the data
+    $sig = base64_url_decode($encoded_sig);
+    $data = json_decode(base64_url_decode($payload), true);
+
+    if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+      error_log('Unknown algorithm. Expected HMAC-SHA256');
+      return null;
+    }
+
+    // check sig
+    $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+    if ($sig !== $expected_sig) {
+      error_log('Bad Signed JSON signature!');
+      return null;
+    }
+
+    return $data;
+  }
+
+  function base64_url_decode($input) {
+    return base64_decode(strtr($input, '-_', '+/'));
+
+  } 
+
+
 print strlen($app_access_token);
 ?>
 
