6e8f2d1c30356da1c233a0bac1562f12d9a57421
Gonzalo <gonzalo@drupro.fi>
2012-02-29 08:23:13 +0200

    Otra

--- index.php
+++ index.php
@@ -47,7 +47,8 @@ if ($user_id) {
     // If the call fails we check if we still have a user. The user will be
     // cleared if the error is because of an invalid accesstoken
     if (!$facebook->getUser()) {
-      header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      //header('Location: '. AppInfo::getUrl($_SERVER['REQUEST_URI']));
+      echo "Please, login in facebook before!!";
       exit();
     }
   }
