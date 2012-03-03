a7d667a21191f756abbb0f1ba63aa095f7fcc637
Gonzalo <gonzalo@drupro.fi>
2012-03-03 09:30:16 +0200

    ok

--- index.php
+++ index.php
@@ -161,7 +161,7 @@ if (isset($_REQUEST['func']) && in_array($_REQUEST['func'],array('scores'))) {
 */				
 
 /*
-user,app_user_access
+*user,app_user_access
 user,app_access
 user,access_token
 
@@ -172,7 +172,7 @@ app,access_token
 */
 
 				$score_URL = 'https://graph.facebook.com/' . $user_id . '/scores';
-  				$score_result = https_post($score_URL,'score=' . $score . '&access_token=' . $_SESSION['fb_app_user_access_token']);  				
+  				$score_result = https_post($score_URL,'score=' . $score . '&access_token=' . $_SESSION['fb_app_access_token']);  				
      	 		if ($score_result) {
      	 			error_log('Resultado del posteo de puntos?: '.var_export($score_result,true));
      	 		} else {
